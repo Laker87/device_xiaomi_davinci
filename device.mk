8df0bb66bac1cd7ed6f848945f8fd97c4dea048d
Laker <lakerman1987@gmail.com>
2020-11-18 21:42:20 +0300

    Revert "Add OPLauncher"
    This reverts commit 93272a96c86c897b4c4adda68cb0ce39995d59c8.

--- device.mk
+++ device.mk
@@ -281,9 +281,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/com.android.nfc_extras.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_davinci/com.android.nfc_extras.xml \
     frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_davinci/com.nxp.mifare.xml
 
-#OnePlus Launcher
-$(call inherit-product, vendor/oplauncher/OPLauncher2.mk)
-
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
