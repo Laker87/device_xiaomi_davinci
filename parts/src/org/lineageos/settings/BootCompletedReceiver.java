505985217eb8a63490ff29442e5f517558ae86b4
Laker <lakerman1987@gmail.com>
2020-12-22 17:33:02 +0300

    davinci: parts: Implement automated DC Dimming
    Based on:
    https://github.com/ritujb/android_frameworks_base/commit/6f1a6ff05e793cb00d8ff0ebb7f0ad113b4af856
    https://github.com/ritujb/android_packages_apps_Settings/commit/f6c912c73a92ac3038b2b15b303c66a96b7694df

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -25,6 +25,7 @@ import android.util.Log;
 import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.display.DcDimmingUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     private static final boolean DEBUG = false;
@@ -37,5 +38,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         DozeUtils.checkDozeService(context);
         PopupCameraUtils.startService(context);
         ThermalUtils.startService(context);
+        DcDimmingUtils.startService(context);
     }
 }
