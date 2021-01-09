9385295319624507e582daf8e16cffda628a35cb
Mathieu Chartier <mathieuc@google.com>
2021-01-09 14:21:27 +0000

    davinci: Speed profile services and wifi-service to reduce RAM and storage
    Change-Id: I1d907b24cb2760ddd9cc65ac4335730c8e61a0a3
    Signed-off-by: karthik558 <karthik.lal558@gmail.com>

--- device.mk
+++ device.mk
@@ -415,6 +415,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.systemhelper@1.0.vendor
 
+# Speed profile services and wifi-service to reduce RAM and storage.
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
