44f45bf3f33e1626f9c4a908e27f5014e5548010
Marco Rizzo <marco.rizzo.00@outlook.com>
2021-01-24 20:32:57 +0300

    davinci: it's ANXCamera time
    Revert "davinci: update GCamGO"
    
    This reverts commit d407e055d34e12332eb39af5c79a2ade32c3c571.
    
    Revert "davinci: add prebuilt Gcam Go"
    
    This reverts commit 011fb0b9ab1f4d8c4e460215ff257bae7c814cd0.
    
    davinci: switch to ANXCamera
    
    Signed-off-by: Marco Rizzo <marco.rizzo.00@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,6 +25,9 @@ BUILD_BROKEN_USES_NETWORK := true
 # Inherit from proprietary files
 include vendor/xiaomi/davinci/BoardConfigVendor.mk
 
+# ANXCamera
+include vendor/aeonax/ANXCamera/BoardConfigAnx.mk
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
