f2bbaf54f3c16ffa4f56455ea3e7e07ee77031f4
Dan Pasanen <dan.pasanen@gmail.com>
2021-01-30 22:51:44 +0300

    Exclude lineage-sdk overlays from RRO

--- device.mk
+++ device.mk
@@ -298,7 +298,8 @@ DEVICE_PACKAGE_OVERLAYS += \
 
 PRODUCT_ENFORCE_RRO_TARGETS += *
 PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-system
+    $(LOCAL_PATH)/overlay-system \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk
 
 # Power
 PRODUCT_PACKAGES += \
