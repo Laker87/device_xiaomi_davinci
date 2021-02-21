9f7d1712bfed4ebd5fa0d4463b4c85bf3c2c4838
Baalaji <me@baalajimaestro.me>
2021-02-21 16:40:44 +0300

    props: Optimise dex flags for a faster boot
    * Used multiple threads and speed profile to hasten the first boot
    
    Signed-off-by: baalajimaestro <me@baalajimaestro.me>
    Change-Id: I2cce5ddf7d50308511e81436fcac613b2c6537bf

--- device.mk
+++ device.mk
@@ -129,6 +129,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+# Dex
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
