3f4d1f2f53cb4a3cb3ef652d105abd543de04b36
Laker <lakerman1987@gmail.com>
2021-02-27 09:11:10 +0300

    RemovePackages: Add empty package instead of null

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -12,7 +12,7 @@ LOCAL_OVERRIDES_PACKAGES := Browser \
 							BATestApp BTTestApp rfc l2test_ertm gatt_tool_qti_internal btsnoop
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_SRC_FILES := /dev/null
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 include $(BUILD_PREBUILT)
 
 ifeq ($(VANILLA),true)
@@ -23,6 +23,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_OVERRIDES_PACKAGES := Updater
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
-LOCAL_SRC_FILES := /dev/null
+LOCAL_SRC_FILES := $(LOCAL_MODULE).apk
 include $(BUILD_PREBUILT)
 endif
