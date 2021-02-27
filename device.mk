cea75b08884f40d369a3637473d7d92f33b9061e
mickaelmendes50 <mesquita@aospa.co>
2021-02-27 15:39:28 +0300

    davinci: Uprev Health hal from 2.0 to 2.1.
    Change-Id: Ib2d1e7742b49919045022d385460e0a3fedf3033

--- device.mk
+++ device.mk
@@ -211,7 +211,9 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
 
 # Storage health HAL
 PRODUCT_PACKAGES += \
