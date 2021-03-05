fd74873c4985ca05687eed20251a0c1b58732625
Laker <lakerman1987@gmail.com>
2021-03-05 21:31:17 +0300

    Revert "Add vndk libprotobuf libs"
    This reverts commit 869de9bddec45d66609731c1f11c8c2a3460e4bf.

--- device.mk
+++ device.mk
@@ -464,13 +464,6 @@ PRODUCT_COPY_FILES += \
 # Vibrator
 $(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
-# VNDK
-PRODUCT_COPY_FILES += \
-		prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-full.so \
-		prebuilts/vndk/v29/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite.so \
-		prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-full.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-full.so \
-		prebuilts/vndk/v29/arm64/arch-arm64-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libprotobuf-cpp-lite.so
-
 # Vulkan
 PRODUCT_PACKAGES += \
     libvulkan
