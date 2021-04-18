2d8dcd8bf311988d92629a615b5ce38119950cf1
Vishalcj17 <vishalcj@aospa.co>
2021-04-18 17:16:21 +0300

    rootdir: Set up/down_rate_limit_us as 1000
    - In my testing, i faced some CPU Stalls with the old setup. (caused some spikes in jitter and non-consistent hackbench).
    
    - 1000 was perfect in my testing and did not hit over power usage.

--- rootdir/bin/init.qcom.post_boot.sh
+++ rootdir/bin/init.qcom.post_boot.sh
@@ -107,8 +107,8 @@ case "$target" in
 
     # configure governor settings for little cluster
     echo "schedutil" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-    echo 500 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
-    echo 5000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
+    echo 1000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/up_rate_limit_us
+    echo 1000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/down_rate_limit_us
     echo 768000 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_freq
     echo 90 > /sys/devices/system/cpu/cpu0/cpufreq/schedutil/hispeed_load
 
@@ -118,8 +118,8 @@ case "$target" in
 
     # configure governor settings for big cluster
     echo "schedutil" > /sys/devices/system/cpu/cpu6/cpufreq/scaling_governor
-    echo 500 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/up_rate_limit_us
-    echo 5000 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/down_rate_limit_us
+    echo 1000 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/up_rate_limit_us
+    echo 1000 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/down_rate_limit_us
     echo 1 > /sys/devices/system/cpu/cpufreq/policy4/schedutil/pl
     echo 806400 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_freq
     echo 90 > /sys/devices/system/cpu/cpu6/cpufreq/schedutil/hispeed_load
