55ab34ee392d084d15d137194a72511a56eb2164
Laker <lakerman1987@gmail.com>
2021-04-23 18:36:54 +0300

    Add permission for power-off alarm framework

--- device.mk
+++ device.mk
@@ -344,10 +344,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/permissions/qti_whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/qti_whitelist.xml \
-    $(LOCAL_PATH)/configs/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml
+    $(LOCAL_PATH)/configs/permissions/system_ext-privapp-permissions-qti.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-permissions-qti.xml \
+    $(LOCAL_PATH)/configs/permissions/vendor-qti-hardware-alarm.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/permissions/vendor-qti-hardware-alarm.xml
 
 # Remove packages
-
 PRODUCT_PACKAGES += \
     RemovePackages \
     RemoveUpdater
