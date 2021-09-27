c2553a46e606808a299e3ba8d7e5fe3b809c4820
Laker <lakerman1987@gmail.com>
2021-09-27 17:11:17 +0300

    Revert "davinci: parts: Switch to TaskStackListener"
    This reverts commit f5d111083575b8be384f0c55733f62e5567bec9b.

--- parts/src/org/lineageos/settings/thermal/ThermalService.java
+++ parts/src/org/lineageos/settings/thermal/ThermalService.java
@@ -17,43 +17,48 @@
 package org.lineageos.settings.thermal;
 
 import android.app.ActivityManager;
-import android.app.ActivityTaskManager;
-import android.app.TaskStackListener;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Handler;
 import android.os.IBinder;
-import android.os.RemoteException;
 import android.util.Log;
+
+import java.util.List;
 
 public class ThermalService extends Service {
     private static final String TAG = "ThermalService";
     private static final boolean DEBUG = false;
 
+    private final Handler mHandler = new Handler();
+
     private String mPreviousApp;
     private ThermalUtils mThermalUtils;
+    private ActivityRunnable mActivityRunnable;
 
     private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            mPreviousApp = "";
-            mThermalUtils.setDefaultThermalProfile();
+            final String action = intent.getAction();
+            if (Intent.ACTION_SCREEN_ON.equals(action)) {
+                mHandler.postDelayed(mActivityRunnable, 500);
+            } else {
+                mHandler.removeCallbacks(mActivityRunnable);
+                mPreviousApp = "";
+                mThermalUtils.setDefaultThermalProfile();
+            }
         }
     };
 
     @Override
     public void onCreate() {
-        if (DEBUG)
-            Log.d(TAG, "Creating service");
-        try {
-            ActivityTaskManager.getService().registerTaskStackListener(mTaskListener);
-        } catch (RemoteException e) {
-            // Do nothing
-        }
+        if (DEBUG) Log.d(TAG, "Creating service");
         mThermalUtils = new ThermalUtils(this);
+        mActivityRunnable = new ActivityRunnable(this);
+        mHandler.postDelayed(mActivityRunnable, 500);
         registerReceiver();
         super.onCreate();
     }
@@ -72,27 +77,31 @@ public class ThermalService extends Service {
 
     private void registerReceiver() {
         IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_SCREEN_ON);
         filter.addAction(Intent.ACTION_SCREEN_OFF);
         this.registerReceiver(mIntentReceiver, filter);
     }
 
-    private final TaskStackListener mTaskListener = new TaskStackListener() {
+    private class ActivityRunnable implements Runnable {
+        private Context context;
+
+        private ActivityRunnable(Context context) {
+            this.context = context;
+        }
+
         @Override
-        public void onTaskStackChanged() {
-            try {
-                final ActivityManager.StackInfo focusedStack =
-                        ActivityTaskManager.getService().getFocusedStackInfo();
-                if (focusedStack != null && focusedStack.topActivity != null) {
-                    ComponentName taskComponentName = focusedStack.topActivity;
-                    String foregroundApp = taskComponentName.getPackageName();
-                    if (!foregroundApp.equals(mPreviousApp)) {
-                        mThermalUtils.setThermalProfile(foregroundApp);
-                        mPreviousApp = foregroundApp;
-                    }
+        public void run() {
+            ActivityManager manager = context.getSystemService(ActivityManager.class);
+            List<ActivityManager.RunningTaskInfo> runningTasks = manager.getRunningTasks(1);
+            if (runningTasks != null && runningTasks.size() > 0) {
+                ComponentName topActivity = runningTasks.get(0).topActivity;
+                String foregroundApp = topActivity.getPackageName();
+                if (!foregroundApp.equals(mPreviousApp)) {
+                    mThermalUtils.setThermalProfile(foregroundApp);
+                    mPreviousApp = foregroundApp;
                 }
-            } catch (Exception e) {
-                // Do nothing
+                mHandler.postDelayed(this, 5000);
             }
         }
-    };
+    }
 }
